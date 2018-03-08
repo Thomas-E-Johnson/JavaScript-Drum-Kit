a2ad6e8f92061c342f24c2a731db128e6226371b
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-03-08 23:37:26 +0000

    changes

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-window.addEventListener(`keydown`, function functionName(e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
-  console.log(key);
-  console.log(audio);
+  key.classList.add(`playing`);
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== `transform`) return;
+  this.classList.remove(`playing`);
+}
+
+document.addEventListener(`click`, function(e) {
+  const clickAudio = document.querySelector(`audio[id="${e.srcElement.innerText}"]`);
+  if(!clickAudio) return;
+  clickAudio.currentTime = 0;
+  clickAudio.play();
+  console.log(e.target.dataset.key);
 });
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener(`transitionend`,removeTransition));
+
+window.addEventListener(`keydown`, playSound);
